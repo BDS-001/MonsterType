dccff1cbd5d0267188c452ea643e11b76a9ae98b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-11 18:03:43 -0400

    update: gameplay scene handles multiple enemies

--- src/game/scenes/gameplayScene.js
+++ src/game/scenes/gameplayScene.js
@@ -6,12 +6,12 @@ export default class GameScene extends Phaser.Scene {
     constructor() {
         super('GameScene');
         this.currentKey = null;
-        this.keyDisplay = null;
         this.player = null
-        this.enemy = null
+        this.enemies
     }
 
     preload() {
+        //preload game assets
         console.log('Preload phase');
         this.load.image('player', 'assets/sprite1.png')
         this.load.image('enemy', 'assets/sprite2.png')
@@ -26,25 +26,28 @@ export default class GameScene extends Phaser.Scene {
             console.log(`Key pressed: ${this.currentKey}`);
         });
 
-        // make a custom class to add player sprite
-        this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
-        this.enemy = new Enemy(this, 200, 200)
+        // setup enmies array using phaser group
+        this.enemies = this.add.group();
+        this.enemies.addMultiple([new Enemy(this, 200, 200), new Enemy(this, 400, 200), new Enemy(this, 600, 200)]);
 
+        //player sprite
+        this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
+
+        //fps counter text
         this.fpsText = this.add.text(10, 10, 'FPS: 0', { 
             font: '16px Arial', 
             fill: '#00ff00' 
         });
-
-        this.keyDisplay = this.add.text(100, 100, '', {
-            font: '32px Arial',
-            fill: '#ffffff'
-        });
     }
 
     update() {
-        this.keyDisplay.text = this.currentKey
         this.fpsText.setText('FPS: ' + Math.round(this.game.loop.actualFps));
-        this.enemy.update(this.currentKey)
+        
+        const currentEnemies = this.enemies.getChildren()
+        for (let i = currentEnemies.length - 1; i >= 0; i--) {
+            currentEnemies[i].update(this.currentKey)
+        }
+
         this.currentKey = null
     }
 }
