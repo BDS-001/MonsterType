9e01b5af67a9b7e447a329fc550beadf2eab1361
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-28 21:43:02 -0400

    add: global gamestate

--- src/game/entities/player.js
+++ src/game/entities/player.js
@@ -14,11 +14,9 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
 		// Set the scale of the player sprite
 		this.setScale(gameSettings.SPRITE_SCALE);
-		this.health = 100;
 	}
 
 	takeDamage() {
-		this.health -= 10;
 		this.setTint(0xff0000);
 		this.scene.time.delayedCall(100, () => {
 			this.clearTint();
