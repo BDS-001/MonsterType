14e0dd006cb93d5ca297e1fc4d270065df571538
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-28 21:43:02 -0400

    add: global gamestate

--- src/game/core/gameState.js
+++ src/game/core/gameState.js
@@ -1,1 +1,12 @@
 //implement state management later
+class GameState {
+  constructor() {
+    this.player = { health: 100, immunity: false };
+  }
+
+  takeDamage(damage) {
+    this.player.health -= damage
+  }
+}
+
+export default new GameState()
