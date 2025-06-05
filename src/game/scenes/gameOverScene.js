2974d66c01bac4fb52c2d746ff6d271bba44b7dd
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-05 18:03:31 -0400

    update: game over scene resets the game scene

--- src/game/scenes/gameOverScene.js
+++ src/game/scenes/gameOverScene.js
@@ -1,14 +1,43 @@
+import gameState from '../core/gameState';
+
 export class GameOver extends Phaser.Scene {
 	constructor() {
 		super({ key: 'GameOver', active: true });
 	}
+
+	preload() {
+		// Load game images
+		this.load.image('playagain', 'assets/playagain.png');
+	}
+
 	create() {
 		this.scene.setVisible(false);
 
 		// transparent backround for the scene so i can still see gameplay
 		this.cameras.main.setBackgroundColor('rgba(0,0,0,0.3)');
 
-		// Add pause-menu UI here (text/buttons), e.g.:
-		this.add.text(0, 0, 'GAMEOVER', { fontSize: '48px', color: '#fff' });
+		// Center the game over text
+		this.add
+			.text(this.game.config.width / 2, this.game.config.width / 2 - 150, 'GAMEOVER', {
+				fontSize: '48px',
+				color: '#fff',
+			})
+			.setOrigin(0.5);
+
+		// Add play again button
+		const playAgainButton = this.add.image(
+			this.game.config.width / 2,
+			this.game.config.width / 2,
+			'playagain'
+		);
+		playAgainButton.setScale(5);
+		playAgainButton.setInteractive();
+		playAgainButton.on('pointerdown', this.playAgain, this);
+	}
+
+	playAgain() {
+		gameState.resetGameState();
+		this.scene.setVisible(false);
+		this.scene.start('GameScene');
 	}
 }
