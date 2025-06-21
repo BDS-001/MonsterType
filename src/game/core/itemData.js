a0cd7e1660c541790795d2fe2a3bff13097a5aad
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-21 15:40:18 -0400

    add: item data

---
+++ src/game/core/itemData.js
@@ -0,0 +1,30 @@
+export const ITEM_DATA = {
+    BOMB: {
+      id: 'BOMB',
+      name: 'Bomb',
+      word: 'explode',
+      type: 'consumable',
+      rarity: 'common',
+    },
+    HEAVYROUNDS_PICKUP: {
+      id: 'HEAVYROUNDS_PICKUP',
+      name: 'Heavy Rounds',
+      word: 'heavy',
+      type: 'weapon',
+      rarity: 'rare'
+    },
+    MEDKIT: {
+      id: 'MEDKIT',
+      name: 'Medkit',
+      word: 'heal',
+      type: 'consumable',
+      rarity: 'common'
+    },
+    HEALTH_UP: {
+      id: 'HEALTH_UP',
+      name: 'Max Health Upgrade',
+      word: 'endurance',
+      type: 'upgrade',
+      rarity: 'rare'
+    }
+  };
---
+++ src/game/entities/items/item.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+import { ITEM_DATA } from '../../core/itemData.js';
+
+export default class Item extends Phaser.Physics.Arcade.Image {
+    constructor(scene, x, y, itemId) {
+        super(scene, x, y, 'item-sprite');
+        
+        const itemData = ITEM_DATA[itemId];
+        if (!itemData) {
+            throw new Error(`Item data not found for id: ${itemId}`);
+        }
+        
+        this.id = itemData.id;
+        this.name = itemData.name;
+        this.word = itemData.word;
+        this.type = itemData.type;
+        this.rarity = itemData.rarity;
+        
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+    }
+}
