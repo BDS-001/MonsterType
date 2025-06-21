44441f74f6b0a076103277b2dc18ca0a6c950141
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-20 23:15:32 -0400

    add: weapon classes for future use

---
+++ src/game/entities/weapons/shotgun.js
@@ -0,0 +1,12 @@
+import Weapon from './weapon.js';
+
+export default class Shotgun extends Weapon {
+	constructor() {
+		super('Shotgun', 'Fires multiple basic projectiles in a spread pattern.', {
+			attackSpeed: 1000,
+			projectileCount: 3,
+			projectileType: 'basicShot',
+			spread: Math.PI / 6 // 30 degree spread
+		});
+	}
+}
