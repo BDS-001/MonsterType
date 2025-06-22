5ce36da0746cfb1a6b97583ddf4e2c5513a44b4c
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-22 14:02:10 -0400

    update: items now extend typedEntity

--- src/game/entities/items/item.js
+++ src/game/entities/items/item.js
@@ -1,14 +1,14 @@
-import Phaser from 'phaser';
+import TypedEntity from '../typedEntity';
 import { ITEM_DATA } from '../../core/itemData.js';
 
-export default class Item extends Phaser.Physics.Arcade.Image {
+export default class Item extends TypedEntity {
 	constructor(scene, x, y, itemId) {
-		super(scene, x, y, 'item-sprite');
-
 		const itemData = ITEM_DATA[itemId];
 		if (!itemData) {
 			throw new Error(`Item data not found for id: ${itemId}`);
 		}
+
+		super(scene, x, y, 'item-sprite', itemData.word);
 
 		this.id = itemData.id;
 		this.name = itemData.name;
--- src/game/managers/itemManager.js
+++ src/game/managers/itemManager.js
@@ -8,38 +8,7 @@ export default class ItemManager {
 		this.setupItems();
 	}
 
-	setupItems() {
-		this.items = this.scene.physics.add.group({
-			maxSize: 10,
-			active: false,
-			visible: false,
-			runChildUpdate: true,
-			createCallback: (item) => {
-				item.scene = this.scene;
-			},
-		});
-	}
-
-	getProjectiles() {
-		return this.projectiles;
-	}
-
-	getProjectile() {
-		let item = this.items.get();
-		if (!item) {
-			item = new this.currentWeaponClass(this.scene, 0, 0);
-			this.items.add(item);
-		}
-		return item;
-	}
-
-	update() {
-		const projectiles = this.projectiles.getChildren();
-		for (let i = 0; i < projectiles.length; i++) {
-			const projectile = projectiles[i];
-			if (projectile.active) {
-				projectile.update();
-			}
-		}
+	spawnItem(x, y, id) {
+		return
 	}
 }
