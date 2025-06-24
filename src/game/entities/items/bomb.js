818ab87a720bb71aceea7a935008c5bc4f3997a7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-24 12:38:44 -0400

    update: JSDoc comments

--- src/game/entities/items/bomb.js
+++ src/game/entities/items/bomb.js
@@ -1,6 +1,22 @@
+/**
+ * Explosive Bomb Item
+ *
+ * Consumable explosive item that can be collected by typing "explode".
+ * Provides area damage capability when used.
+ */
 import Item from './item.js';
 
+/**
+ * Explosive consumable item
+ * Deals area damage when activated
+ */
 export default class Bomb extends Item {
+	/**
+	 * Create a new bomb item
+	 * @param {Phaser.Scene} scene - The scene this bomb belongs to
+	 * @param {number} x - X position for bomb placement
+	 * @param {number} y - Y position for bomb placement
+	 */
 	constructor(scene, x, y) {
 		super(scene, x, y, 'BOMB');
 	}
--- src/game/entities/items/heavyRoundsPickup.js
+++ src/game/entities/items/heavyRoundsPickup.js
@@ -1,6 +1,22 @@
+/**
+ * Heavy Rounds Weapon Pickup Item
+ *
+ * Weapon upgrade pickup that can be collected by typing "heavy".
+ * Provides enhanced ammunition or weapon capabilities when collected.
+ */
 import Item from './item.js';
 
+/**
+ * Heavy rounds weapon upgrade pickup
+ * Enhances weapon damage or ammunition capacity
+ */
 export default class HeavyRoundsPickup extends Item {
+	/**
+	 * Create a new heavy rounds pickup item
+	 * @param {Phaser.Scene} scene - The scene this heavy rounds pickup belongs to
+	 * @param {number} x - X position for heavy rounds pickup placement
+	 * @param {number} y - Y position for heavy rounds pickup placement
+	 */
 	constructor(scene, x, y) {
 		super(scene, x, y, 'HEAVYROUNDS_PICKUP');
 	}
