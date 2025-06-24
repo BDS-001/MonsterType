2fcd178dcaad3e02ef50afbc0b45662d9c460063
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-24 12:38:44 -0400

    update: JSDoc comments

--- src/game/entities/items/healthUp.js
+++ src/game/entities/items/healthUp.js
@@ -1,6 +1,22 @@
+/**
+ * Health Upgrade Item
+ *
+ * Permanent health increase upgrade that can be collected by typing "endurance".
+ * Provides a permanent boost to maximum health when collected.
+ */
 import Item from './item.js';
 
+/**
+ * Permanent health upgrade item
+ * Increases maximum health capacity when collected
+ */
 export default class HealthUp extends Item {
+	/**
+	 * Create a new health upgrade item
+	 * @param {Phaser.Scene} scene - The scene this health upgrade belongs to
+	 * @param {number} x - X position for health upgrade placement
+	 * @param {number} y - Y position for health upgrade placement
+	 */
 	constructor(scene, x, y) {
 		super(scene, x, y, 'HEALTH_UP');
 	}
--- src/game/entities/items/medkit.js
+++ src/game/entities/items/medkit.js
@@ -1,6 +1,22 @@
+/**
+ * Medical Kit Item
+ *
+ * Consumable health restoration item that can be collected by typing "heal".
+ * Provides immediate health recovery when used.
+ */
 import Item from './item.js';
 
+/**
+ * Health restoration consumable item
+ * Restores health when collected and used
+ */
 export default class Medkit extends Item {
+	/**
+	 * Create a new medkit item
+	 * @param {Phaser.Scene} scene - The scene this medkit belongs to
+	 * @param {number} x - X position for medkit placement
+	 * @param {number} y - Y position for medkit placement
+	 */
 	constructor(scene, x, y) {
 		super(scene, x, y, 'MEDKIT');
 	}
