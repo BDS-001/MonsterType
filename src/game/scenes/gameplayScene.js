2b620eb86f22c35168d8f6cd484edf5d291ea182
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-08-25 19:43:04 -0400

    format: prettier

--- src/game/scenes/gameplayScene.js
+++ src/game/scenes/gameplayScene.js
@@ -27,7 +27,7 @@ export default class GameScene extends Phaser.Scene {
 		this.load.image('mummy', 'assets/enemies/mummy.png');
 		this.load.image('grass', 'assets/environment/grass.png');
 		this.load.image('basicShot', 'assets/weapons/basicShot.png');
-		
+
 		// Load item sprites
 		this.load.image('bomb', 'assets/items/bomb.png');
 		this.load.image('medkit', 'assets/items/medkit.png');
